d9c52e84ddc70f97c401f728c59811591b19a11c
MiSo <jimrayder3701@gmail.com>
2022-02-15 09:37:39 +0900

    Add #1 - add domain and constant package
    api 에 필요한 domain, enum(constant) 구현

---
+++ src/main/java/com/fastcampus/programming/getinline/constant/EventStatus.java
@@ -0,0 +1,5 @@
+package com.fastcampus.programming.getinline.constant;
+
+public enum EventStatus {
+    PENDING, OPENED, CLOSED, CANCELLED, ABORTED;
+}
---
+++ src/main/java/com/fastcampus/programming/getinline/constant/PlaceType.java
@@ -0,0 +1,5 @@
+package com.fastcampus.programming.getinline.constant;
+
+public enum PlaceType {
+    COMMON, SPORTS, RESTAURANT, PARTY;
+}
