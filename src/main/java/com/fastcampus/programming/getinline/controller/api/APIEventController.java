228b9a34ec92040797ff89f5edb1d55b5d82b1d4
MiSo <jimrayder3701@gmail.com>
2022-02-16 15:12:46 +0900

    Fix #1 - Fided APIEventController
    test 에서 계속 실패했던 APIEventController 수정 및 validation 적용

--- src/main/java/com/fastcampus/programming/getinline/controller/api/APIEventController.java
+++ src/main/java/com/fastcampus/programming/getinline/controller/api/APIEventController.java
@@ -2,18 +2,23 @@ package com.fastcampus.programming.getinline.controller.api;
 
 import com.fastcampus.programming.getinline.constant.EventStatus;
 import com.fastcampus.programming.getinline.dto.APIDataResponse;
+import com.fastcampus.programming.getinline.dto.EventRequest;
 import com.fastcampus.programming.getinline.dto.EventResponse;
 import com.fastcampus.programming.getinline.service.EventService;
-import com.sun.jdi.request.EventRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Validated
 @RequiredArgsConstructor
 @RequestMapping("/api")
 @RestController
@@ -23,8 +28,8 @@ public class APIEventController {
 
     @GetMapping("/events")
     public APIDataResponse<List<EventResponse>> getEvents(
-            Long placeId,
-            String eventName,
+            @Positive Long placeId,
+            @Size(min = 2) String eventName,
             EventStatus eventStatus,
             @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime eventStartDatetime,
             @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime eventEndDatetime
@@ -42,10 +47,11 @@ public class APIEventController {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/events")
-    public APIDataResponse<Void> createEvent(
-            @RequestBody EventRequest eventRequest
+    public APIDataResponse<String> createEvent(
+            @Valid @RequestBody EventRequest eventRequest
     ) {
-        return APIDataResponse.empty();
+        boolean result = eventService.createEvent(eventRequest.toDTO());
+        return APIDataResponse.of(Boolean.toString(result));
     }
 
     @GetMapping("/events/{eventId}")
